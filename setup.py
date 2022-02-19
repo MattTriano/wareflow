6bef11cbcf2c6fd57cef017f3c6e813999bfab1e
matttriano <matt.triano@gmail.com>
2022-02-18 20:01:21 -0600

    Modified setup.py to require sqlalchemy 1.4 or higher.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as reqs_file:
+	requirements = reqs_file.readlines()
+
 setup(
 	author="Matt Triano",
     name="wareflow",
 	description="Tools for managing a personal data warehouse",
     version="0.1.0",
 	packages=find_packages(),
-	include_package_data=True
+	include_package_data=True,
+    install_requirements=requirements
 )
